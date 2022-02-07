d250bc46b4fd0e522f4a45e9e77fe39417d54606
Vinícius Venceslau <92820372+ViniciusVenceslau@users.noreply.github.com>
2022-02-06 23:01:59 -0300

    Update figurageometrica.h

--- figurageometrica.h
+++ figurageometrica.h
@@ -2,14 +2,19 @@
 #define FIGURAGEOMETRICA_H
 #include "sculptor.h"
 /**
- *classe abstrata que serve para representar objetos primitivos genericos.
+ *@Classe abstrata que serve para representar objetos primitivos genericos.
+ *@Diversos tipos de formas então inseridas dentro do campo figura geométrica.
+ *@É a classe "mãe" para os tipos de figuras geométricas que serão criadas. Neste contexto, as classses concretas (filhas).
  */
 class FiguraGeometrica{
 protected:
     float r, g, b, alpha;
 public:
-    virtual ~FiguraGeometrica(){};
-    virtual void draw(Sculptor &s)=0;
+    virtual ~FiguraGeometrica(){}; // Destrutor
+    virtual void draw(Sculptor &s)=0; /*
+                                       *@Função Virtual Pura.
+                                       *@"virtual" -> Faz com que o programa execute os métodos das subclasses e não da superclasse.
+                                      */
 };
 
 #endif // FIGURAGEOMETRICA_H
